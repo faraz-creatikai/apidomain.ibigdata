a5a7f087f0c237b2c7cda55de85d1ee7d2d3c67b
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-01-29 12:18:48 +0530

    u

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://property.ibigdata.in",
+      "https://domain.ibigdata.in",
       
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
