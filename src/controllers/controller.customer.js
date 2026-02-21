1d9f1465af8eeb76bcbebabb49ec727a3b955b43
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-02-21 11:55:51 +0530

    u

--- src/controllers/controller.customer.js
+++ src/controllers/controller.customer.js
@@ -291,7 +291,7 @@ export const getCustomer = async (req, res, next) => {
     if (ContactNumber) AND.push({ ContactNumber: { contains: ContactNumber.trim() } });
     if (ReferenceId) AND.push({ ReferenceId: { contains: ReferenceId.trim() } });
     if (Price) AND.push({ Price: { contains: Price.trim() } });
-    if (isFavourite) AND.push({ isFavourite: { contains: isFavourite } });
+    if (isFavourite) AND.push({ isFavourite: { contains: isFavourite === "true" } });
 
     // --------------------------------------------
     // KEYWORD SEARCH
