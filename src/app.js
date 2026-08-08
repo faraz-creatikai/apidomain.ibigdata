9e9267c29ced67fa77eb13f3389fd881fd39bdfb
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-08-08 19:50:13 +0530

    u

--- src/app.js
+++ src/app.js
@@ -60,6 +60,7 @@ app.use(
   cors({
     origin: ALLOWED_ORIGINS,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
