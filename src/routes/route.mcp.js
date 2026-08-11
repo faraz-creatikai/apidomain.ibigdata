c4ab97afd8f01d6f50f154eae9da2817aa0e83d2
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-08-11 11:35:59 +0530

    U

--- src/routes/route.mcp.js
+++ src/routes/route.mcp.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createCustomerJson } from "../controllers/mcpController.js";
+import { createCustomerJson, importCustomersJson } from "../controllers/mcpController.js";
 
 const mcpRoutes = express.Router();
 
 
 mcpRoutes.post("/create/customer", createCustomerJson);
+mcpRoutes.post("/import/json", importCustomersJson);
 
 export default mcpRoutes;
